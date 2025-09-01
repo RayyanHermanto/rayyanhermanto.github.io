838fbd76a7c503c2f2d5338215eb57ab1f9c5b40
Rayyan <60314224+RayyanHermanto@users.noreply.github.com>
2025-09-02 01:25:10 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -62,7 +62,7 @@ for (let i = 0; i < testimonialsItem.length; i++) {
     // Jika title dan text sama-sama kosong, isi modalText dengan "blabla"
     if (titleHTML === "Tanam AI" && checktextHTML === "Frontend") {
       modalText.innerHTML = '<p class="project-category">For the frontend of Tanam AI, I developed it during a coding camp by Dicoding, powered by DBS. It was built as a Single Page Application (SPA) to deliver smooth and seamless navigation, while the implementation of Progressive Web App (PWA) features ensures offline functionality and installability. With a responsive design, the application adapts well across devices, showcasing modern web development practices and practical use cases. This frontend highlights innovation, efficiency, and adaptability gained through hands-on learning.</p>';
-      modalGithub.href = "https://github.com/RayyanHermanto/TanamAI"; 
+      modalGithub.href = "https://github.com/tatangwarianta/TanamAI"; 
       modalTime.textContent = "17 June, 2025";
       modalTime.setAttribute("datetime", "2025-06-17");
     }else if (titleHTML === "Tanam AI" && checktextHTML === "Backend"){
